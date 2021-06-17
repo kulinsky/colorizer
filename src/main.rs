153501e811f8a84472019e5a34ccac78f3d6830e
kulinsky <ankulinsky@gmail.com>
2021-06-17 09:00:00 +0300

    add errors handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::io;
 use std::fs::File;
 use std::collections::HashMap;
-use std::io::{BufRead, Read};
+use std::io::{BufRead, Read, Write};
 
 use clap::Clap;
 use serde::Deserialize;
 use serde_json::{Map, Value};
+use anyhow::{anyhow, Context, Result};
 use ansi_term::Colour::{Blue, Cyan, Yellow, Red, Green, Purple};
 
 /// Simple program to colorize any word
@@ -13,7 +14,7 @@ use ansi_term::Colour::{Blue, Cyan, Yellow, Red, Green, Purple};
 #[clap(name = "colorizer", version = "0.1.0")]
 struct CliConfig {
     /// Path to config.json file
-    #[clap(short, long,  default_value = "~/.colorizer/settings.json")]
+    #[clap(short, long)]
     config: String,
 
     /// Profile from config to use
@@ -27,26 +28,30 @@ struct Config {
 }
 
 impl Config {
-    fn automatic() -> Result<Config, &'static str> {
+    fn automatic() -> Result<Config> {
         let args = CliConfig::parse();
 
-        let mut file = File::open(args.config).unwrap();
+        let mut file = File::open(&*args.config)
+            .with_context(|| format!("Failed to open config from {}", &*args.config))?;
         let mut buff = String::new();
-        file.read_to_string(&mut buff).unwrap();
+        file.read_to_string(&mut buff)
+            .with_context(|| format!("Failed to read config file {}", &*args.config))?;
 
-        let parsed: Value = serde_json::from_str(&buff).unwrap();
-        let obj: &Map<String, Value> = parsed.as_object().unwrap();
+        let parsed: Value = serde_json::from_str(&buff)
+            .with_context(|| format!("Failed to parse json {}", args.config))?;
+        let obj: &Map<String, Value> = parsed.as_object()
+            .with_context(|| format!("Failed to parse json to object {}", &*args.config))?;
 
-        if obj.contains_key(&*args.profile) {
-            let val = obj.get(&*args.profile).unwrap().clone();
-            return Ok(serde_json::from_value(val).unwrap())
-        }
+        let val = obj.get(&*args.profile)
+            .with_context(|| format!("profile not found `{}`", &*args.profile))?.clone();
 
-        return Err("profile not found");
+        let config = serde_json::from_value(val)?;
+
+        Ok(config)
     }
 }
 
-fn colorize(color: &str, word: &str) -> Result<String, &'static str> {
+fn colorize(color: &str, word: &str) -> Result<String> {
     match color {
         "RED" => Ok(Red.paint(word).to_string()),
         "GREEN" => Ok(Green.paint(word).to_string()),
@@ -54,20 +59,24 @@ fn colorize(color: &str, word: &str) -> Result<String, &'static str> {
         "CYAN" => Ok(Cyan.paint(word).to_string()),
         "YELLOW" => Ok(Yellow.paint(word).to_string()),
         "PURPLE" => Ok(Purple.paint(word).to_string()),
-        _ => Err("unknown color ")
+        _ => Err(anyhow!("unknown color: {}", color))
     }
 }
 
-fn main() {
-    let conf = Config::automatic().unwrap();
+fn main() -> Result<()> {
+    let conf = Config::automatic()?;
+    let stdout = io::stdout();
+    let mut buff = io::BufWriter::new(stdout.lock());
 
     for line in io::stdin().lock().lines() {
         let mut line = line.expect("Could not read line from standard in");
 
         for k in conf.substrings.keys() {
-            line = line.replace(k, &*colorize(&*conf.substrings[k], k).unwrap())
+            line = line.replace(k, &*colorize(&*conf.substrings[k], k)?)
         }
 
-        println!("{}", line);
-    }
+        writeln!(&mut buff, "{}", line).ok();
+    };
+
+    Ok(())
 }
